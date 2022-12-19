0c8cf8d54bd9b144309d49836b36569e3448f0f9
Hazem-Farhati <hazemfarhati@gmail.com>
2022-12-19 21:24:16 +0100

    vid fl wasset

--- frontend/src/pages/View.js
+++ frontend/src/pages/View.js
@@ -15,19 +15,41 @@ const View = ({
 }) => {
     const product = useSelector((state) => state.product?.product);
       const { id } = useParams();
- 
+ const [upId, setUpId] = useState(id)
   return (
-
-    <div className="view">
-      {product
-        ?.filter((el) => id == el._id)
-        .map((el, i) => (
-          <>
-            <h4 style={{ color: "red" }}>{el?.title}</h4>
-            <iframe src={el.video} frameborder="0"></iframe>
-          </>
-        ))
-        .reverse()}
+    <div className="all_view">
+      {console.log(upId)}
+      <div className="view">
+        {product
+          ?.filter((el) => id == el._id)
+          .map((el, i) => (
+            <>
+              <h4 style={{ color: "red" }}>{el?.title}</h4>
+              <iframe src={el.video} frameborder="0"></iframe>
+            </>
+          ))
+          .reverse()}
+      </div>
+      <div className="view_right_side">
+        {product
+          ?.filter((el) =>
+            el.title?.toLowerCase().includes(search.toLowerCase())
+          )
+          .map((el) => (
+            <div className="v_vid">
+              <CardVideo
+                onClick={()=>setUpId(id)}
+                setwidth1={setwidth1}
+                el={el}
+                width1={width1}
+                width={width}
+                show={show}
+                setShow={setShow}
+              />
+            </div>
+          ))
+          .reverse()}
+      </div>
     </div>
   );
 };
